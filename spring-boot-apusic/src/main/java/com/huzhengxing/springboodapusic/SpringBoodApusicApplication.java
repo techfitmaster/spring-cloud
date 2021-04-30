79844276a7e0184dfd99239c1d346aca16c1f1ca
Albert <1092144169@qq.com>
2021-04-30 11:08:49 +0800

    Testing apusic

---
+++ spring-boot-apusic/src/main/java/com/huzhengxing/springboodapusic/SpringBoodApusicApplication.java
@@ -0,0 +1,13 @@
+package com.huzhengxing.springboodapusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoodApusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoodApusicApplication.class, args);
+    }
+
+}
