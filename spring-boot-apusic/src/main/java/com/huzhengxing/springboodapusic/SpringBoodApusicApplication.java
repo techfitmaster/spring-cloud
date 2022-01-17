c4538ccb65e60aa3319ae4320563c26f2b3e225c
Albert <1092144169@qq.com>
2022-01-17 17:38:52 +0800

    grpc

--- spring-boot-apusic/src/main/java/com/huzhengxing/springboodapusic/SpringBoodApusicApplication.java
+++ spring-boot-apusic/src/main/java/com/huzhengxing/springboodapusic/SpringBoodApusicApplication.java
@@ -2,12 +2,26 @@ package com.huzhengxing.springboodapusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringBoodApusicApplication {
+@RestController
+public class SpringBoodApusicApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBoodApusicApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBoodApusicApplication.class, args);
     }
 
+
+    @GetMapping
+    public String test() {
+        return "Request success";
+    }
 }
---
+++ spring-boot-grpc/README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: zhengxing.hu
+ * @Date: 2022-01-12 10:41:38
+ * @LastEditTime: 2022-01-12 10:45:55
+ * @Description: 
+-->
+
+
+
